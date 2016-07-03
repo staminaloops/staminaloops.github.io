47d6afd00063faa50734f5b7e44311df9330999a
staminaloops <staminaloops@gmail.com>
2016-07-03 06:39:07 +0100

    change the forms to Formspree

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,23 +5,36 @@ if (!(window.location.host.startsWith("localhost")) && (window.location.protocol
 
 //highligth.js
 $(document).ready(function() {
-    $('pre').each(function(i, block) {
-        hljs.highlightBlock(block);
-    });
+  $('pre').each(function(i, block) {
+    hljs.highlightBlock(block);
+  });
 });
 
 
 $(document).ready(function() {
 
-    $('form').on('submit', function() {
-        // Success message
-        $('#success').html("<div class='alert alert-success'>");
-        $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
-        $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
-        $('#success > .alert-success')
-            .append('</div>');
+  $('form').on('submit', function(e) {
+    e.preventDefault();
+    $('#submitButton').attr('disabled');
+    var data = $(this).serialize()
+    $.ajax({
+      url: "https://formspree.io/staminaloops@gmail.com",
+      method: "POST",
+      data: data,
+      dataType: "json"
+    }).done(function() {
+      // Success message
+      $('#success').html("<div class='alert alert-success'>");
+      $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+        .append("</button>");
+      $('#success > .alert-success')
+        .append("<strong>Your message has been sent. </strong>");
+      $('#success > .alert-success')
+        .append('</div>');
+      // reset
+      document.getElementById('form3').reset();
+      $('#submitButton').removeAttr('disabled');
+    })
 
-    });
+  });
 });
